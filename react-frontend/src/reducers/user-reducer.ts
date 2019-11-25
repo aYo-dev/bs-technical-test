ddf17e63bca12609674f7e6933233c21b4edfdf8
Asen Yovchev <ayovcheff@gmail.com>
2019-11-25 13:04:00 +0200

    update user reducer to update on proper way the state when a user is deleted and updated

--- react-frontend/src/reducers/user-reducer.ts
+++ react-frontend/src/reducers/user-reducer.ts
@@ -1,6 +1,6 @@
 import { combineReducers, AnyAction } from 'redux';
 
-import { GetUsersActionTypes, CreateUserActionTypes } from '../actions/action-types';
+import { GetUsersActionTypes, CreateUserActionTypes, DeleteUserActionTypes, EditUserActionTypes } from '../actions/action-types';
 import { User } from '../interfaces/user.interface';
 
 export type UsersState = Readonly<{
@@ -17,8 +17,15 @@ export default combineReducers<UsersState, AnyAction>({
       case GetUsersActionTypes.GET_USERS_SUCCESS:
         return [...action.payload];
     
-      case CreateUserActionTypes.CREATE_USERS_SUCCESS:
+      case CreateUserActionTypes.CREATE_USER_SUCCESS:
         return [...state, ...action.payload];
+
+      case DeleteUserActionTypes.DELETE_USER_SUCCESS:
+        return state.filter((u) => u.id !== action.payload);
+
+      case EditUserActionTypes.EDIT_USER_SUCCESS:
+          return state.map((u) => u.id === action.payload.id ? action.payload : u);
+
       default:
         return state;
     }
