d321515e366bca4fa6f75a33f2c0dc7a183e742a
Asen Yovchev <ayovcheff@gmail.com>
2019-11-25 12:41:03 +0200

    add routes for edit and create user

--- react-frontend/src/routes/index.tsx
+++ react-frontend/src/routes/index.tsx
@@ -3,10 +3,20 @@ import { Route, Switch } from 'react-router';
 
 // Components
 import { Home } from '../components';
+import { UserForm } from '../components/UserForm/UserForm';
 
-const routes = ( 
+const routes = () => ( 
   <Switch>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/">
+      <Home />
+    </Route>
+    <Route path="/create">
+      <UserForm />
+    </Route>
+    <Route path="/edit/:userId">
+      <UserForm />
+    </Route>
+    <Route path="*" render={() => (<div>Not Found</div>)} />
   </Switch>
 );
 
